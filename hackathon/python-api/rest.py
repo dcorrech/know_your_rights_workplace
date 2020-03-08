37347e584f929e6e2df4c7b977a81a4d0d49e178
dcorrech <damacorrech@gmail.com>
2020-03-08 11:56:36 -0700

    fixed api task to store results from get_similar_text

--- hackathon/python-api/rest.py
+++ hackathon/python-api/rest.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, abort, request
 from to_csv import get_value
 from get_similar_text import get_most_similar_text
 import csv
+from flask_cors import CORS, cross_origin
 
 # add call to to_csv function to load csv
 values = get_value()
@@ -17,25 +18,34 @@ with open('results.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     for row in csvReader:
         result.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
+        id = row[0]
+        what_happened = row[1]
+        employer = row[2]
+        reason = row[3]
+        province = row[4]
+        details = row[5]
+        resolution = row[6]
 
 
 app = Flask(__name__)
-
-cors = CORS(app, resources={r"/tasks": {"origins": "http://localhost:5000"}})
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app, resources={r"/task": {"origins": "http://localhost:5000"}})
 
 tasks = [
     {
-        'id': values[x][0],
-        'what_happened': values[x][1],
-        'employer': values[x][2],
-        'reason': values[x][3],
-        'province': values[x][4],
-        'details': values[x][5],
-        'resolution': values[x][6],
+        'id': id,
+        'what_happened': what_happened,
+        'employer': employer,
+        'reason': reason,
+        'province': province,
+        'details': details,
+        'resolution': resolution,
     }
 ]
 
 @app.route('/tasks', methods = ['GET'])
+@cross_origin()
 def get_tasks():
     return jsonify( {'tasks': tasks})
 
